1abcae0f01dba444fb9c7f569b48cdc32292f29f
loc-cv <donbb6824@gmail.com>
2022-09-21 10:39:51 +0700

    Minor change for prettier rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -35,6 +35,6 @@ module.exports = {
     'plugin:prettier/recommended',
   ],
   rules: {
-    'prettier/prettier': ['warn', {}, { usePrettierrc: true }],
+    'prettier/prettier': 'warn',
   },
 };
